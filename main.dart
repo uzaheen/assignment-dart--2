fe4171c4157fe0b0bb562320498ff1c14c45136c
uzaheen <81876961+uzaheen@users.noreply.github.com>
2021-11-24 23:49:54 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+void main(){
+  // stdout.write("City Name: ");
+  // var d = (stdin.readByteSync());
+  // if  (d== "karachi");
+  // print("Wellcom to city of Lights");
+
+//  stdout.write("remaining fuel: ");
+//     var d = (stdin.readByteSync());
+//    if  (d== "2.25");
+//    print("your fuel is not sufficent");
+
+// stdout.write("weather: ");
+// var d = (stdin.readByteSync());
+// var t = 40;
+// if (t == 40) { 
+// print("it is too hot outside"); 
+//     } 
+// var t2 = 30;
+// if (t2 == 30) { 
+// print("The weather is today normal"); 
+//     } 
+// var t3 = 20;
+// if (t3 == 20) { 
+// print("Today weather is cool"); 
+// }
+// var t4 = 10;
+// if (t4 == 10) { 
+// print("omg today weather is cool"); 
+// } else {
+//   print("error");
+// }
+
+
+}
